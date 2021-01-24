4577f0c73c29ec6bb0ea05e594643839d8b3083e
HubUser99 <cliff.goga@gmail.com>
2021-01-24 22:28:48 +0200

    Added blurhash to be displayed

--- src/components/RestaurantCard/CardImage.tsx
+++ src/components/RestaurantCard/CardImage.tsx
@@ -4,6 +4,8 @@ import {
     CardMedia,
     Typography,
 } from "@material-ui/core";
+import { useState } from "react";
+import { Blurhash } from "react-blurhash";
 
 interface Props {
     src: string;
@@ -12,11 +14,35 @@ interface Props {
 }
 
 export const CardImage = ({ src, name, online }: Props) => {
-    const classes = useStyles();
+    const [isLoading, setIsLoading] = useState(true);
+
+    const classes = useStyles(isLoading);
+
+    const handleChangeIsLoading = () => {
+        setIsLoading(false);
+    };
 
     return (
         <div className={classes.imageContainer}>
-            <CardMedia component="img" height={200} src={src} title={name} />
+            <CardMedia
+                component="img"
+                height={200}
+                src={src}
+                title={name}
+                onLoad={handleChangeIsLoading}
+                className={classes.cardImage}
+            />
+            {isLoading && (
+                <Blurhash
+                    hash="LEHV6nWB2yk8pyo0adR*.7kCMdnj"
+                    width="100%"
+                    height={200}
+                    resolutionX={32}
+                    resolutionY={32}
+                    punch={1}
+                    className={classes.blurhashImage}
+                />
+            )}
             {online ? null : (
                 <div className={classes.imageOverlay}>
                     <Typography variant="h6">Temporarily offline</Typography>
@@ -42,6 +68,16 @@ const useStyles = makeStyles((theme) =>
             justifyContent: "center",
             color: "#FFF",
         },
+        blurhashImage: {
+            position: "absolute",
+            bottom: 0,
+            width: "100%",
+            height: "100%",
+        },
+        cardImage: (isLoading) => ({
+            visibility: isLoading ? "collapse" : "unset",
+            height: isLoading ? 0 : undefined,
+        }),
     })
 );
 
